01da1cd430c2d492f2e38d44c96856bfc22aacbc
alex mickelson <alexmickelson96@gmail.com>
2019-02-28 18:38:24 -0700

    Building Nested Components

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,17 +1,28 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { IProduct } from "./Product";
 
 @Component ({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
 
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List!';
     imageWidth: number=50;
     imageMargin: number=2;
     showImage: boolean = false;
-    listFilter: string='cart';
-    products: any[] = [
+    _listFilter: string;
+    get listFilter(): string {
+      return this._listFilter;
+    }
+    set listFilter(value:string) {
+      this._listFilter = value;
+      this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+
+    filteredProducts: IProduct[];
+    products: IProduct[] = [
         {
         "productId": 1,
         "productName": "Leaf Rake",
@@ -33,7 +44,26 @@ export class ProductListComponent {
         "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
       }
     ];
+
+    constructor() {
+      this.filteredProducts = this.products;
+      this.listFilter = 'cart';
+    }
+
     toggleImage(): void{
         this.showImage = !this.showImage;
+    }
+    performFilter(filteredBy: string): IProduct[] {
+      filteredBy = filteredBy.toLocaleLowerCase();
+      return this.products.filter((product: IProduct) => 
+          product.productName.toLocaleLowerCase().indexOf(filteredBy) !== -1);
+    }
+
+    onRatingClicked(message: string): void{
+      this.pageTitle = 'ProductList '+ message;
+    }
+
+    ngOnInit(): void {
+      console.log('product-list initialized');
     }
 }
